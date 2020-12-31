231dff33626b7a453911cb0fc7b084a69baf7d31
Greg Barkway <greg.barkway@gmail.com>
2020-12-30 18:46:12 -0800

    Still broken

--- src/capture-serv/captureService.js
+++ src/capture-serv/captureService.js
@@ -2,7 +2,7 @@ const captures = require('./captures.json')
 const dates = new Set(captures.map(c => c.date))
 
 getCaptureByDate = date => captures.find(c => c.date == date.toUpperCase())
-getDates = () => [1999-11-05]
+getDates = () => ['1999-11-05']
 hasDate = date => dates.has(date.toUpperCase());
 
 module.exports = { getCaptureByDate, hasDate, getDates }
