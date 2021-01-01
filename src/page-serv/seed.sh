d31202b66be33f9ac5eca1626ea1f7a7dd52a755
Greg Barkway <greg.barkway@gmail.com>
2021-01-01 10:57:12 -0800

    make dockerfiles saner

--- src/page-serv/seed.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-mongoimport --db hypnospace --collection pages --drop -h $MONGO_HOST --file ./pages.json
-mongoimport --db hypnospace --collection captures --drop -h $MONGO_HOST --file ./captures.json
