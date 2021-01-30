3f19571847cb9fb37e7213a18a0993273ba137d9
Greg Barkway <greg.barkway@gmail.com>
2021-01-30 00:40:01 -0800

    kind of working d3 sitemap

--- src/web/src/Sitemap.js
+++ src/web/src/Sitemap.js
@@ -1,24 +1,27 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { Graph } from "react-d3-graph";
 
 export default function Sitemap({ date }) {
     const [data, setData] = useState({
-        nodes: [{id: "Test1"}, {id: "Test2"}],
+        nodes: [],
         links: [
-            { source: "Test1", target: "Test2"}
         ]
     })
     
+    const a = useRef();
+
     useEffect(() => {
         fetch(`http://localhost:3001/captures/${date}`)
             .then((res) => {
                 return res.json();
             })
             .then((capture) => {
-                return {
+                const thing = {
                     nodes: capture.pages.map((page) => ({id: page.path})),
                     links: capture.links.map((link) => ({source: link.sourcePath, target: link.targetPath}))
                 }
+                console.log(thing);
+                return thing;
             })
             .then((asdf) => {
                 setData(asdf);
@@ -28,15 +31,10 @@ export default function Sitemap({ date }) {
 
     // the graph configuration, just override the ones you need
     const myConfig = {
-        nodeHighlightBehavior: true,
-        node: {
-        color: "lightgreen",
-        size: 120,
-        highlightStrokeColor: "blue",
-        },
-        link: {
-        highlightColor: "lightblue",
-        },
+        height: 800,
+        d3: {
+            //disableLinkForce: true,
+        }
     };
     
     const onClickNode = function(nodeId) {
@@ -51,7 +49,6 @@ export default function Sitemap({ date }) {
             id="graph-id" // id is mandatory
             data={data}
             config={myConfig}
-            onClickNode={onClickNode}
-            onClickLink={onClickLink}
+            ref={a}
             />);
 }
