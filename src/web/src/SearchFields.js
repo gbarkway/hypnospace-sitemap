ca4d0211fcb3bd7c4a2e3503e22a4c796ef87511
Greg Barkway <greg.barkway@gmail.com>
2021-02-03 20:01:20 -0800

    Webapp - submit search on Enter

--- src/web/src/SearchFields.js
+++ src/web/src/SearchFields.js
@@ -4,20 +4,23 @@ import {Form, Button} from "react-bootstrap";
 export default function SearchFields({onSearchClicked, searchFields, onSearchFieldsChange}) 
 { 
     return (
-        <Form>
+        <Form onSubmit={(e) => {
+            e.preventDefault();
+            onSearchClicked(searchFields);
+        }}>
             <Form.Group>
                 <Form.Label>Page name or description:</Form.Label>
-                <Form.Control value={searchFields.pageNameQuery} onChange={(e) => onSearchFieldsChange({...searchFields, pageNameQuery: e.target.value})} type="text" placeholder="Page name"/>
+                <Form.Control value={searchFields.pageNameQuery} onChange={(e) => onSearchFieldsChange({ ...searchFields, pageNameQuery: e.target.value })} type="text" placeholder="Page name" />
             </Form.Group>
             <Form.Group>
                 <Form.Label>User name:</Form.Label>
-                <Form.Control value={searchFields.userNameQuery} onChange = {(e) => onSearchFieldsChange({...searchFields, userNameQuery: e.target.value})} type="text" placeholder="User name" />
+                <Form.Control value={searchFields.userNameQuery} onChange={(e) => onSearchFieldsChange({ ...searchFields, userNameQuery: e.target.value })} type="text" placeholder="User name" />
             </Form.Group>
             <Form.Group>
                 <Form.Label>Tags:</Form.Label>
-                <Form.Control value={searchFields.tagsQuery} onChange = {(e) => onSearchFieldsChange({...searchFields, tagsQuery: e.target.value})} type="text" placeholder="tag1,tag2,tag3" />
+                <Form.Control value={searchFields.tagsQuery} onChange={(e) => onSearchFieldsChange({ ...searchFields, tagsQuery: e.target.value })} type="text" placeholder="tag1,tag2,tag3" />
             </Form.Group>
-            <Button variant="primary" onClick={() => onSearchClicked(searchFields)}>Search Now</Button>
+            <Button variant="primary" type="submit">Search Now</Button>
         </Form>
     )
 }
