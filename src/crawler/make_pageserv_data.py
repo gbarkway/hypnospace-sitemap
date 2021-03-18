45b5238a861b4cf00b44a4e89f7fc40d4fa734ce
Greg Barkway <greg.barkway@gmail.com>
2021-03-17 21:58:37 -0700

    Crawler: fix flake8 errors (#17)

--- src/crawler/make_pageserv_data.py
+++ src/crawler/make_pageserv_data.py
@@ -3,26 +3,39 @@ import sys
 from pathlib import Path
 import crawler
 
+
 def pageServPages(capture):
-    zoneNames = {page.zone: page.name for page in capture.pages if page.isZoneHome}
-    pageServPage = lambda page: {
-        'path': page.path,
-        'zone': zoneNames[page.zone],
-        'date': capture.date,
-        'name': page.name,
-        'description': page.description or '',
-        'tags': page.tags,
-        'user': page.user
+    zoneNames = {
+        page.zone: page.name
+        for page in capture.pages if page.isZoneHome
     }
 
+    def pageServPage(page):
+        return {
+            'path': page.path,
+            'zone': zoneNames[page.zone],
+            'date': capture.date,
+            'name': page.name,
+            'description': page.description or '',
+            'tags': page.tags,
+            'user': page.user
+        }
+
     return [pageServPage(page) for page in capture.pages]
+
 
 def pageServCaptures(captures):
     return [{'date': c.date} for c in captures]
 
+
 if len(sys.argv) < 2:
     print('Usage: python make_pageserv_data.py [PATH]')
-    print('Path must be the path to a Hypnospace Outlaw data folder. For example: C:\\Program Files (x86)\\Steam\\steamapps\\common\\Hypnospace Outlaw\\data')
+    print(
+        'Path must be the path to a Hypnospace Outlaw data folder. ',
+        'For example:',
+        ' C:\\Program Files (x86)\\Steam\\steamapps\\common\\',
+        'Hypnospace Outlaw\\data'
+    )
     exit()
 
 dataPath = Path(sys.argv[1])
@@ -32,7 +45,9 @@ if not dataPath.exists():
 
 hypnospace = crawler.readHypnospace(dataPath)
 
-pages = [page for capture in hypnospace.captures for page in pageServPages(capture)]
+pages = [
+    page for capture in hypnospace.captures for page in pageServPages(capture)
+]
 outPath = Path('./pageserv.pages.json')
 with open(outPath, 'w') as file:
     file.writelines((json.dumps(p) + '\n' for p in pages))
@@ -44,5 +59,7 @@ with open(outPath, 'w') as file:
     file.writelines((json.dumps(c) + '\n' for c in pageServCaptures))
 print(f'Output written to ${outPath.resolve()}')
 
-print(f'Copy these files to ../page-serv/db/pages.json and ../page-serv/db/captures.json')
-
+print(
+    'Copy these files to ../page-serv/db/pages.json and ',
+    '../page-serv/db/captures.json'
+)
