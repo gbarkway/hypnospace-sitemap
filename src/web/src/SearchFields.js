bbebf58153f0f614b0cb69cc4bd9da7025833d15
Greg Barkway <greg.barkway@gmail.com>
2021-04-25 12:32:10 -0700

    Revert "Web: auto-focus first search input"
    This reverts commit 07b2b18bec63b737e65c0cf9276d625cde27570c.
    Caused keyboard to popup every time on mobile which was very
    annoying

--- src/web/src/SearchFields.js
+++ src/web/src/SearchFields.js
@@ -37,7 +37,6 @@ export default function SearchFields({
             }
             type="text"
             id="nameOrDescriptionField"
-            autoFocus
           />
         </Form.Group>
         <Form.Group>
