f64494cbc4e31b5eb491970e47eccc05b9d5ebbf
Greg Barkway <greg.barkway@gmail.com>
2021-04-24 21:21:06 -0700

    Break out some pagedetails into smaller components

--- src/web/src/PageDetails.js
+++ src/web/src/PageDetails.js
@@ -38,6 +38,36 @@ function Tags({tags, onTagClick}) {
   )
 }
 
+function SpecialLinksListGroupItem({linkedByAd, linkedByMail}) {
+  if (!linkedByAd && !linkedByMail) {
+    return null;
+  }
+
+  return (
+    <ListGroup.Item className="p-2">
+    { linkedByAd ? <div><img src={adIcon} width="16" height="16" className="mx-1" alt=""/>Linked by ad or popup</div> : null }
+    { linkedByMail ? <div><img src={mailIcon} className="mx-1" alt=""></img>Linked by HypnoMail</div> : null }
+</ListGroup.Item>
+  )
+}
+
+function DefaultPageDetails({error}) {
+  return (
+    <div className="pageDetails h-100">
+      <Card className="square h-100">
+        <Card.Header>
+          <h5>Page Details</h5>
+        </Card.Header>
+        <Card.Body>
+          <Card.Text>
+            {error.length ? error : "No page selected. Click a zone to get started."}
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </div>
+  );
+}
+
 //TODO: indicate if a page is linked to by hypnomail or by an ad
 export default function PageDetails({ date, path, onTagClick, onCitizenNameClick }) {
   onTagClick = onTagClick || (() => {});
@@ -80,62 +110,44 @@ export default function PageDetails({ date, path, onTagClick, onCitizenNameClick
       .finally(() => setLoading(false));
   }, [date, path]);
 
-  if (page) {
-    return (
-      <div className="pageDetails h-100">
-        <Card className="square h-100">
-          <Card.Header>
-            <h5>Page Details - {page.name}</h5>
-          </Card.Header>
-          <Card.Body style={{ overflowY: "scroll" }}>
-            <div className="d-flex justify-content-between">
-              <b>{page.name}</b>
-              <Spinner visible={loading} />
-            </div>
-            {/* If viewed on platform that doesn't display scrollbars, and card too short to display all info at once, 
-              cut-off listbox provides visual indication that users should scroll to see more */}
-            <ListGroup className="my-2">
-              <ListGroup.Item className="p-2">{page.path}</ListGroup.Item>
-              <ListGroup.Item className="p-2">
-                <b>Zone:</b> {page.zone || <MutedNoneText />}
-              </ListGroup.Item>
-              <ListGroup.Item className="p-2">
-                <b>Citizen:</b>
-                <Button onClick={() => onCitizenNameClick(page.citizenName)} variant="link">
-                  {page.citizenName || "???"}
-                </Button>
-              </ListGroup.Item>
-              <ListGroup.Item className="p-2">
-                <b>Description:</b> {page.description || <MutedNoneText />}
-              </ListGroup.Item>
-              <ListGroup.Item className="p-2">
-                <Tags tags={page.tags} onTagClick={onTagClick}/>
-              </ListGroup.Item>
-              { page.linkedByAd || page.linkedByMail ? (
-                              <ListGroup.Item className="p-2">
-                              { page.linkedByAd ? <div><img src={adIcon} width="16" height="16" className="mx-1" />Linked by ad or popup</div> : null }
-                              { page.linkedByMail ? <div><img src={mailIcon} className="mx-1"></img>Linked by HypnoMail</div> : null }
-                          </ListGroup.Item>
-              ) : null}
-            </ListGroup>
-          </Card.Body>
-        </Card>
-      </div>
-    );
-  } else {
-    return (
-      <div className="pageDetails h-100">
-        <Card className="square h-100">
-          <Card.Header>
-            <h5>Page Details</h5>
-          </Card.Header>
-          <Card.Body>
-            <Card.Text>
-              {error.length ? error : "No page selected. Click a zone to get started."}
-            </Card.Text>
-          </Card.Body>
-        </Card>
-      </div>
-    );
+  if (!page) {
+    return <DefaultPageDetails error={error}/>;
   }
+
+  return (
+    <div className="pageDetails h-100">
+      <Card className="square h-100">
+        <Card.Header>
+          <h5>Page Details - {page.name}</h5>
+        </Card.Header>
+        <Card.Body style={{ overflowY: "scroll" }}>
+          <div className="d-flex justify-content-between">
+            <b>{page.name}</b>
+            <Spinner visible={loading} />
+          </div>
+          {/* If viewed on platform that doesn't display scrollbars, and card too short to display all info at once, 
+            cut-off listbox provides visual indication that users should scroll to see more */}
+          <ListGroup className="my-2">
+            <ListGroup.Item className="p-2">{page.path}</ListGroup.Item>
+            <ListGroup.Item className="p-2">
+              <b>Zone:</b> {page.zone || <MutedNoneText />}
+            </ListGroup.Item>
+            <ListGroup.Item className="p-2">
+              <b>Citizen:</b>
+              <Button onClick={() => onCitizenNameClick(page.citizenName)} variant="link">
+                {page.citizenName || "???"}
+              </Button>
+            </ListGroup.Item>
+            <ListGroup.Item className="p-2">
+              <b>Description:</b> {page.description || <MutedNoneText />}
+            </ListGroup.Item>
+            <ListGroup.Item className="p-2">
+              <Tags tags={page.tags} onTagClick={onTagClick}/>
+            </ListGroup.Item>
+            <SpecialLinksListGroupItem linkedByAd={page.linkedByAd} linkedByMail={page.linkedByMail} />
+          </ListGroup>
+        </Card.Body>
+      </Card>
+    </div>
+  );
 }
