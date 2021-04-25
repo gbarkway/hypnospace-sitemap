3075f37a47de41783a01624ca684100c2627e175
Greg Barkway <greg.barkway@gmail.com>
2021-04-24 18:13:27 -0700

    Fix typo in spinner test

--- src/web/src/tests/Spinner.test.js
+++ src/web/src/tests/Spinner.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import SearchResults from "../SearchResults";
+import Spinner from "../Spinner";
 
 it("renders without crashing", () => {
   const div = document.createElement("div");
